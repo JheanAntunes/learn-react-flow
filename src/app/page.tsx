065b5b6d21d7168c3460ad3e655b4151aa74170f
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-17 13:00:57 -0300

    Update Home component to use ReactiveFlow and comment out BasicFlow and Flow components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import BasicFlow from "./components/flow/basic-flow";
+import Flow from "./components/flow/custom-node";
+import NodeToolbarExample from "./components/flow/NodeToolbarExample";
+import ReactiveFlow from "./components/multiple-edges";
 
 export default function Home() {
   return (
     <main className="w-svw h-svh">
       {/* <FirstFlow /> */}
       {/* <NodeToolbarExample /> */}
-      <BasicFlow />
+      {/* <BasicFlow /> */}
+      {/* <Flow /> */}
+      <ReactiveFlow />
     </main>
   );
 }
