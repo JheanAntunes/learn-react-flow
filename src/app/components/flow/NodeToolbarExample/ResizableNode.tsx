552f9607d3c68a7a93e40749237d4381e8c02873
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-17 11:57:51 -0300

    Enhance ResizableNode component by adding source handle and adjusting layout

--- src/app/components/flow/NodeToolbarExample/ResizableNode.tsx
+++ src/app/components/flow/NodeToolbarExample/ResizableNode.tsx
@@ -7,7 +7,9 @@ const ResizableNode = ({ data }) => {
     <>
       <NodeResizer minWidth={100} minHeight={30} />
       <Handle type="target" position={Position.Left} />
+
       <div style={{ padding: 10 }}>{data.label}</div>
+
       <Handle type="source" position={Position.Right} />
     </>
   );
