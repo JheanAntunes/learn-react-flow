a7f699ca19a3523f1926a70da24482f06fb031c1
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-17 20:01:25 -0300

    Add Squircle button to menu for adding nodes in the FlowWithZustand component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Squircle } from "lucide-react";
 import FlowWithZustand from "./components/flow-with-zustand";
 import { ModeToggle } from "./components/toggle-theme";
 
 export default function Home() {
   return (
     <main className="w-svw h-svh">
-      <menu className="fixed top-0">
+      <menu className="fixed z-[1001] flex justify-center items-center right-0 left-0 top-5 gap-5">
         <li>
-          <Button size="icon"></Button>
+          <Button size="icon">
+            <Squircle className="h-[1.2rem] w-[1.2rem]" />
+            <span className="sr-only">add node</span>
+          </Button>
+        </li>
+        <li>
           <ModeToggle />
         </li>
       </menu>
