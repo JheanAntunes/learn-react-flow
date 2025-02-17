dff7fa1d51637b7124e9711815985c7543dc2969
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-17 20:01:11 -0300

    Enhance FlowWithZustand component with additional ReactFlow features and improved node visualization

--- src/app/components/flow-with-zustand/index.tsx
+++ src/app/components/flow-with-zustand/index.tsx
@@ -1,7 +1,16 @@
 "use client";
-import { ReactFlow } from "@xyflow/react";
+import {
+  Background,
+  BackgroundVariant,
+  Controls,
+  MiniMap,
+  ReactFlow,
+  SelectionMode,
+} from "@xyflow/react";
 import { useShallow } from "zustand/react/shallow";
 
+import nodeColor from "@/app/util/node-color";
+import { defaultEdgeOptions } from "../flow/default-edges-options";
 import useStore from "./store";
 import { AppState } from "./type";
 
@@ -17,7 +26,6 @@ function FlowWithZustand() {
   const { nodes, edges, onNodesChange, onEdgesChange, onConnect } = useStore(
     useShallow(selector)
   );
-
   return (
     <ReactFlow
       nodes={nodes}
@@ -25,8 +33,18 @@ function FlowWithZustand() {
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
+      selectionOnDrag // Enable selection on drag
+      panOnScroll // Enable pan on scroll
+      connectionRadius={40} // Set the connection radius
+      panOnDrag={[1, 2]} // Set the pan on drag
       fitView
-    />
+      selectionMode={SelectionMode.Partial} // Set the selection mode
+      defaultEdgeOptions={defaultEdgeOptions}
+    >
+      <Background variant={BackgroundVariant.Cross} />
+      <MiniMap className="dark:bg-background" nodeColor={nodeColor} />
+      <Controls className="dark:text-neutral-950" />
+    </ReactFlow>
   );
 }
 
