b3a6f3170d1e029479005bee5d47d6dd409de7e3
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-18 11:25:54 -0300

    feat: Add addNode method to useStore for dynamic node addition

--- src/app/components/flow-with-zustand/store.ts
+++ src/app/components/flow-with-zustand/store.ts
@@ -1,9 +1,9 @@
+import { addEdge, applyEdgeChanges, applyNodeChanges } from "@xyflow/react";
 import { create } from "zustand";
-import { addEdge, applyNodeChanges, applyEdgeChanges } from "@xyflow/react";
 
-import initialNodes from "@/app/util/initial-nodes";
 import initialEdges from "@/app/util/initial-edges";
-import { AppState } from "./type";
+import initialNodes from "@/app/util/initial-nodes";
+import { AppNode, AppState } from "./type";
 
 // this is our useStore hook that we can use in our components to get parts of the store and call actions
 const useStore = create<AppState>()((set, get) => ({
@@ -29,6 +29,9 @@ const useStore = create<AppState>()((set, get) => ({
   },
   setEdges: (edges) => {
     set({ edges });
+  },
+  addNode: (newNode: AppNode) => {
+    get().setNodes([...get().nodes, newNode]);
   },
 }));
 
