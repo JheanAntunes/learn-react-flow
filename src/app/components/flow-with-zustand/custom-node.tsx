a3a7bad92070498c8112b5d20e225ad9ebadc700
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-17 21:55:59 -0300

    Add CustomNode component with local storage integration for node labels

---
+++ src/app/components/flow-with-zustand/custom-node.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import CustomNodeStorage from "./customNodeStorage";
+
+interface CustomNodeProps {
+  id: string;
+  data: {
+    label: string;
+  };
+}
+
+const localStorageKey = "nodes";
+
+const CustomNode: React.FC<CustomNodeProps> = ({ id, data }) => {
+  return (
+    <CustomNodeStorage
+      id={id}
+      content={data.label}
+      localStorageKey={localStorageKey}
+    >
+      {(content, handleChange) => (
+        <div style={{ border: "1px solid black", padding: "10px" }}>
+          <textarea value={content} onChange={handleChange} />
+        </div>
+      )}
+    </CustomNodeStorage>
+  );
+};
+
+export default CustomNode;
