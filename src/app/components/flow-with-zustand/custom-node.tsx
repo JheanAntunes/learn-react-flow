6b9dd366dbbcb96f91590905c899d01ff8ef5ebe
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-18 00:32:33 -0300

    Refactor CustomNode component to simplify structure and enhance styling for Input

--- src/app/components/flow-with-zustand/custom-node.tsx
+++ src/app/components/flow-with-zustand/custom-node.tsx
@@ -1,10 +1,8 @@
 import { Input } from "@/components/ui/input";
-import { Handle, NodeResizeControl, Position } from "@xyflow/react";
-import { ArrowRightFromLine } from "lucide-react";
 import React from "react";
 import CustomNodeStorage from "./customNodeStorage";
 
-interface CustomNodeProps {
+export interface CustomNodeProps {
   id: string;
   data: {
     label: string;
@@ -15,37 +13,21 @@ const localStorageKey = "nodes";
 
 const CustomNode: React.FC<CustomNodeProps> = ({ id, data }) => {
   return (
-    <>
-      <NodeResizeControl
-        style={{ background: "transparent", border: "none" }}
-        minHeight={44}
-        maxHeight={44}
-        minWidth={150}
-        position="bottom-right"
-      >
-        <ArrowRightFromLine
-          className="absolute text-background dark:text-background tranform -translate-x-3.5 -translate-y-3.5"
-          size={14}
-        />
-      </NodeResizeControl>
-      <Handle type="target" position={Position.Left} />
-      <CustomNodeStorage
-        id={id}
-        content={data.label}
-        localStorageKey={localStorageKey}
-      >
-        {(content, handleChange) => (
-          <div className="relative tranform -translate-y-1">
-            <Input
-              className="h-11 border-none"
-              value={content}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-      </CustomNodeStorage>
-      <Handle type="source" position={Position.Right} />
-    </>
+    <CustomNodeStorage
+      id={id}
+      content={data.label}
+      localStorageKey={localStorageKey}
+    >
+      {(content, handleChange) => (
+        <div className="w-full h-full relative flex  items-center justify-center">
+          <Input
+            className="h-10 border-none bg-transparent text-white"
+            value={content}
+            onChange={handleChange}
+          />
+        </div>
+      )}
+    </CustomNodeStorage>
   );
 };
 
