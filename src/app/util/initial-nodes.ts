03c3c91f47d4bf93c98719f94aceea676a5b35ae
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-18 00:47:00 -0300

    Refactor initial nodes configuration to include new node types and adjust positions

--- src/app/util/initial-nodes.ts
+++ src/app/util/initial-nodes.ts
@@ -1,8 +1,14 @@
 import { Node } from "@xyflow/react";
-import CustomNode from "../components/flow-with-zustand/custom-node";
+import NodeCustom from "../components/flow-with-zustand/nodes/node-custom";
+import NodeInput from "../components/flow-with-zustand/nodes/node-input";
+import NodeOutput from "../components/flow-with-zustand/nodes/node-output";
+import NodeProcessing from "../components/flow-with-zustand/nodes/node-processing";
 
 export const nodeTypes = {
-  custom: CustomNode,
+  input: NodeInput,
+  processing: NodeProcessing,
+  output: NodeOutput,
+  custom: NodeCustom,
 };
 
 const initialNodes: Node[] = [
@@ -10,7 +16,8 @@ const initialNodes: Node[] = [
     id: "1",
     type: "input",
     data: { label: "Input Node" },
-    position: { x: 250, y: 5 },
+    position: { x: 100, y: 5 },
+    height: 44,
     style: { backgroundColor: "#6ede87", color: "white" },
   },
   {
@@ -19,20 +26,22 @@ const initialNodes: Node[] = [
     // you can also pass a React component as a label
     data: { label: "Another Node" },
     position: { x: 100, y: 100 },
+    height: 44,
     style: { backgroundColor: "#ff0072", color: "white" },
   },
   {
     id: "3",
     type: "output",
     data: { label: "Output Node" },
-    position: { x: 250, y: 200 },
+    position: { x: 100, y: 200 },
+    height: 44,
     style: { backgroundColor: "#6865A5", color: "white" },
   },
   {
     id: "4",
     type: "custom",
     data: { label: "Custom Node" },
-    position: { x: 250, y: 500 },
+    position: { x: 100, y: 300 },
     height: 44,
     style: { backgroundColor: "#f9a825", color: "white" },
   },
