cdac364e80ea0cf5af98a9b4a6269f7734624996
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-18 00:10:31 -0300

    Replace Textarea with Input component and add node resize controls in CustomNode

--- src/app/components/flow-with-zustand/custom-node.tsx
+++ src/app/components/flow-with-zustand/custom-node.tsx
@@ -1,4 +1,6 @@
-import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { Handle, NodeResizeControl, Position } from "@xyflow/react";
+import { ArrowRightFromLine } from "lucide-react";
 import React from "react";
 import CustomNodeStorage from "./customNodeStorage";
 
@@ -13,17 +15,37 @@ const localStorageKey = "nodes";
 
 const CustomNode: React.FC<CustomNodeProps> = ({ id, data }) => {
   return (
-    <CustomNodeStorage
-      id={id}
-      content={data.label}
-      localStorageKey={localStorageKey}
-    >
-      {(content, handleChange) => (
-        <div style={{ border: "1px solid black", padding: "10px" }}>
-          <Textarea value={content} onChange={handleChange} />
-        </div>
-      )}
-    </CustomNodeStorage>
+    <>
+      <NodeResizeControl
+        style={{ background: "transparent", border: "none" }}
+        minHeight={44}
+        maxHeight={44}
+        minWidth={150}
+        position="bottom-right"
+      >
+        <ArrowRightFromLine
+          className="absolute text-background dark:text-background tranform -translate-x-3.5 -translate-y-3.5"
+          size={14}
+        />
+      </NodeResizeControl>
+      <Handle type="target" position={Position.Left} />
+      <CustomNodeStorage
+        id={id}
+        content={data.label}
+        localStorageKey={localStorageKey}
+      >
+        {(content, handleChange) => (
+          <div className="relative tranform -translate-y-1">
+            <Input
+              className="h-11 border-none"
+              value={content}
+              onChange={handleChange}
+            />
+          </div>
+        )}
+      </CustomNodeStorage>
+      <Handle type="source" position={Position.Right} />
+    </>
   );
 };
 
