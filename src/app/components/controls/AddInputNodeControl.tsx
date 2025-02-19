dce492252a03f5814d72b922e84485d72686fe72
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-19 11:10:32 -0300

    feat: Use generateRandomPosition for dynamic node placement in AddInputNodeControl

--- src/app/components/controls/AddInputNodeControl.tsx
+++ src/app/components/controls/AddInputNodeControl.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { NewNode } from "@/app/types/NewNode";
+import { generateRandomPosition } from "@/app/util/generate-random-position";
 import { Button } from "@/components/ui/button";
 import { Squircle } from "lucide-react";
 import { v4 as uuidv4 } from "uuid";
@@ -10,7 +11,7 @@ const AddInputNodeControl = () => {
     addNode({
       id: uuidv4(),
       type: "input",
-      position: { x: Math.random() * 400, y: Math.random() * 400 },
+      position: generateRandomPosition(),
       data: { label: "New Node Input" },
       height: 44,
       style: { backgroundColor: "#6ede87", color: "white" },
